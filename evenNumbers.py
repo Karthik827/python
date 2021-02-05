97d99bd5beaebb3d7a013bfc8bd5f19700c617f4
karthik827 <karthikponnam78@gmail.com>
2021-02-05 08:24:41 +0530

    evenNumbers program added

---
+++ evenNumbers.py
@@ -0,0 +1,13 @@
+# def even(l1):
+#     result = []
+#     for i in l1:
+#         if i %2 == 0:
+#             result.append(i)
+#     return result
+
+# print(even([2,4,5,6]))
+
+# if i % 2 == 0
+# if not i%2
+# if not 0 
+print([i for i in [3,4,5,6,7,8] if not i % 2])
